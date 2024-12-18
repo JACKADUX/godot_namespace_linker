900f0c12a685ccc21fa2024b5c8d9f4a4f01e41c
JACKADUX <1505648942@qq.com>
2024-12-18 21:40:31 +0800

    init

--- test/test_main.gd
+++ test/test_main.gd
@@ -4,5 +4,9 @@ extends Node
 func _ready() -> void:
 	var foo = NS_Any.Foo.new()
 	var foo2 = NS_Any.SubNamespace.AlsoFoo.new()
+	var bar = NS_Any.SubNamespace.Bar.new()
 	var scene = NS_Any.FOO_SCENE.instantiate()
 	
+	var foo3 = NS_Any2.Foo.new()
+	var foo4 = NS_Any2.SubNamespace.AlsoFoo.new()
+	var scene2 = NS_Any2.FOO_SCENE.instantiate()
--- test/test_scene/_namespace_any2.gd
+++ test/test_scene/_namespace_any2.gd
@@ -1,9 +1,10 @@
-class_name NS_Any extends NAMESPACE
+class_name NS_Any2 extends NAMESPACE
 
 const Foo = preload("res://test/test_scene/foo.gd")
+const Bar = preload("res://test/test_scene/bar.gd")
 const FOO_SCENE = preload("res://test/test_scene/foo_scene.tscn")
 
 class SubNamespace:
 	const AlsoFoo = preload("res://test/test_scene/foo.gd")
-	const Bar = preload("res://test/test_scene/bar.gd")
+	
 	
